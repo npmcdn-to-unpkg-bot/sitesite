d8c870c4e59906737661362bf8f7b194f14e9922
Maksim <akiam@mail.ru>
2016-07-08 19:06:46 +0300

    1. JS file devided by ready blocks to prevent error on another pages 2. Added filter for masonry 3. Fully responsive

--- public/js/main.js
+++ public/js/main.js
@@ -1,110 +1,130 @@
+// Header Fixed
 $(document).ready(function () {
+    var $navToFixed=$('.header');
 
-    var $buttonBtn = $('.btn');
-    var $buttonServices = $('.services__button');
-    var $servicesShowMore = $('.services__item__show-more');
-
-    var $portfolioLink = $('.portfolio-link');
-    var $masonryContainer=$('.portfolio__items');
-    var $portfolioItem = $('.portfolio__item');
-    // Header Scroll
     $(window).on('scroll', function () {
         var scroll = $(window).scrollTop();
 
         if (scroll >= 50) {
-            $('#header').addClass('fixed');
+            $('body').css('margin-top',$navToFixed.outerHeight()+'px');
+            $navToFixed.addClass('fixed-menu');
         } else {
-            $('#header').removeClass('fixed');
+            $navToFixed.removeClass('fixed-menu');
+            $('body').css('margin-top',0);
         }
     });
 
-    // contact form
-    $("#contactsform").submit(function () {
-        var a = $(this).attr("action");
-        $("#message").slideUp(750, function () {
-            $("#message").hide();
-            $("#submit-contacts").attr("disabled", "disabled");
-            $.post(a, {
-                name: $("#contacts-form-name").val(),
-                email: $("#contacts-form-email").val(),
-                phone: $("#contacts-form-phone").val(),
-                comments: $("#contacts-form-message").val()
-            }, function (a) {
-                document.getElementById("message").innerHTML = a;
-                $("#message").slideDown("slow");
-                $("#submit-contacts").removeAttr("disabled");
-                if (null != a.match("success")) $("#contactsform").slideDown("slow");
+});
+
+// contact form sender
+$(document).ready(function () {
+
+    function formSender(form, failMessage) {
+        $(form).submit(function () {
+            var a = $(this).attr("action");
+
+            $(failMessage).slideUp(750, function () {
+                $(failMessage).hide();
+                $(form).attr("disabled", "disabled");
+                $.post(a, {
+                    name: $(form + "__name").val(),
+                    phone: $(form + "__phone").val(),
+                    message: $(form + "__message").val()
+                }, function (a) {
+                    $(failMessage).html(a);
+                    $(failMessage).slideDown("slow");
+                    if (null != a.match("success")) $(form).slideDown("slow");
+                });
             });
+            return false;
         });
-        return false;
-    });
-    $("#contactsform input, #contactsform textarea").keyup(function () {
-        $("#message").slideUp(1500);
+        $(form + " input").keyup(function () {
+            $(failMessage).slideUp(1500);
+        });
+    }
+
+    formSender('.form_contacts','.form__message');
+});
+
+
+//menu scrolling
+$(document).ready(function () {
+
+    $(".menu-link").click(function (event) {
+        event.preventDefault();
+        console.log($(this).attr('href'));
+        var selectorName=$(this).attr('href');
+        var $selector=$(selectorName);
+        $('html,body').animate({scrollTop: $selector.offset().top - 50}, 1000);
     });
 
-    //menu scrolling
-    $(".scroll").click(function (event) {
-        event.preventDefault();
-        $('html,body').animate({scrollTop: $(this.hash).offset().top - 64}, 1000);
-    });
+});
+
+//Show more in services
+//
+// $(document).ready(function () {
+//
+//     var $buttonBtn = $('.btn');
+//     var $buttonServices = $('.services__button');
+//     var $servicesShowMore = $('.services__item__show-more');
+//
+//
+//     $buttonServices.on('click', function () {
+//         var items = '.services__items';
+//         var item = '.services__item';
+//         var showMoreBlock = '.services__item__show-more';
+//
+//         $servicesShowMore.slideUp();
+//
+//         var showMore = $(this).closest(item).find(showMoreBlock);
+//
+//         if (showMore.is(':hidden')) {
+//             showMore.slideDown();
+//             $(items).css('align-items', 'flex-start');
+//         } else {
+//             showMore.slideUp(function () {
+//                 $(items).css('align-items', 'stretch');
+//             });
+//
+//         }
+//     });
+// });
+
+//masonry grid + filter by data-*
+$(document).ready(function () {
+
+    var $portfolioLink = $('.portfolio-link');
+    var $masonryContainer=$('.portfolio__items');
+    var $portfolioItem = $('.portfolio__item');
 
     //masonry
     $masonryContainer.imagesLoaded(function(){
         $masonryContainer.masonry({
             itemSelector: '.portfolio__item',
             columnWidth: '.portfolio_sizer',
-            percentPosition: true,
-            isAnimated: true,
-            transitionDuration: '0.2s'
+            percentPosition: true
         });
     });
 
     $portfolioLink.on('click', function () {
         var attribute = $(this).html().toLowerCase();
-        var i;
-        console.log("ТЭГ НАЧАЛЬНЫЙ: "+attribute);
+        // console.log("ТЭГ НАЧАЛЬНЫЙ: "+attribute);
         $portfolioItem.show();
         $portfolioItem.map(function (i, e) {
             if (attribute == 'все работы') {
                 $portfolioItem.show();
             } else if ($(e).data('tag').toLowerCase().indexOf(attribute) == -1) {
                 //console.log('ЭЛЕМЕНТ: '+e);
-                console.log('ТЭГИ: '+$(e).data('tag'));
-                console.log("ОТВЕТ: "+$(e).data('tag').toLowerCase().indexOf(attribute) > -1);
+                // console.log('ТЭГИ: '+$(e).data('tag'));
+                // console.log("ОТВЕТ: "+$(e).data('tag').toLowerCase().indexOf(attribute) > -1);
                 $(e).hide();
             }
 
             $masonryContainer.masonry({
                 itemSelector: '.portfolio__item',
                 columnWidth: '.portfolio_sizer',
-                percentPosition: true,
-                isAnimated: true,
-                transitionDuration: '0.2s'
+                percentPosition: true
             },'reload');
         });
-
-
-    });
-
-    //Show more in services
-    $buttonServices.on('click', function () {
-        var items = '.services__items';
-        var item = '.services__item';
-        var showMoreBlock = '.services__item__show-more';
-
-        $servicesShowMore.slideUp();
-
-        var showMore = $(this).closest(item).find(showMoreBlock);
-
-        if (showMore.is(':hidden')) {
-            showMore.slideDown();
-            $(items).css('align-items', 'flex-start');
-        } else {
-            showMore.slideUp(function () {
-                $(items).css('align-items', 'stretch');
-            });
-
-        }
     });
 });
-
